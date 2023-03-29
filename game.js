866006769b0f77f3638f833eb51f372f6e8bda53
ZuevIvan <zueviv4@gmail.com>
2023-03-30 02:15:15 +0300

    complete html and css

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 function game() {
     let mN = Number(prompt(`Введите номер месяца`));
-    if (mN >= 1 && mN <= 2 || mN === 12 ) {return console.log(`Это зима`);} 
-    else if (mN >= 3 && mN <= 5 ) {return console.log(`Это весна`);}
-    else if (mN >= 6 && mN <= 8 ) {return console.log(`Это лето`);}
-    else if (mN >= 9 && mN <= 11 ) {return console.log(`Это осень`);}
-    else if (isNaN(mN) === true ) {return console.log(`Ты втираешь мне какую-то ДИЧЬ!!!`);}   
+    if (mN >= 1 && mN <= 2 || mN === 12 ) {return alert(`Это зима`);} 
+    else if (mN >= 3 && mN <= 5 ) {return alert(`Это весна`);}
+    else if (mN >= 6 && mN <= 8 ) {return alert(`Это лето`);}
+    else if (mN >= 9 && mN <= 11 ) {return alert(`Это осень`);}
+    else if (isNaN(mN) === true ) {return alert(`Ты втираешь мне какую-то ДИЧЬ!!!`);}   
 }
