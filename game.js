9bc159d133054a73caf8c0ac33444b4471575365
ZuevIvan <zueviv4@gmail.com>
2023-03-31 15:02:36 +0300

    completed hw 7, but not game.js

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
-function game() {
+function gameFirst() {
     let mN = Number(prompt(`Введите номер месяца`));
     if (mN >= 1 && mN <= 2 || mN === 12 ) {return alert(`Это зима`);} 
     else if (mN >= 3 && mN <= 5 ) {return alert(`Это весна`);}
     else if (mN >= 6 && mN <= 8 ) {return alert(`Это лето`);}
     else if (mN >= 9 && mN <= 11 ) {return alert(`Это осень`);}
     else if (isNaN(mN) === true ) {return alert(`Ты втираешь мне какую-то ДИЧЬ!!!`);}   
+}
+
+function gameSecomd(){
+    let fruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    let remember = alert(`$[fruit]`);
+    
 }
