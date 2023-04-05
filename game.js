3262280803ad511c064a0f2588c4680dc1adb6ba
ZuevIvan <zueviv4@gmail.com>
2023-04-05 15:55:14 +0300

    completed secondGame

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-function game() {
+function gameFirst() {
     let mN = Number(prompt(`Введите номер месяца`));
     if (mN >= 1 && mN <= 2 || mN === 12 ) {return alert(`Это зима`);} 
     else if (mN >= 3 && mN <= 5 ) {return alert(`Это весна`);}
@@ -6,3 +6,21 @@ function game() {
     else if (mN >= 9 && mN <= 11 ) {return alert(`Это осень`);}
     else if (isNaN(mN) === true ) {return alert(`Ты втираешь мне какую-то ДИЧЬ!!!`);}   
 }
+
+function gameSecond(){
+    const fruits = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'].sort(() => Math.random() - 0.5)
+
+    alert(fruits)
+    
+    let firstUserAnswer = (String(prompt("Чему равнялся первый элемент массива?"))).toLowerCase();
+    
+    let secondUserAnswer = (String(prompt("Чему равнялся последний элемент массива?"))).toLowerCase();
+    
+    let firstElem = fruits[0].toLowerCase();
+    let secondElem = fruits[6].toLowerCase();
+    if (firstUserAnswer === firstElem && secondUserAnswer === secondElem) {alert("Все верно!");} 
+    else if (firstUserAnswer === firstElem || secondUserAnswer === secondElem) {alert("Вы были близки к победе!");}
+    else if (firstUserAnswer != firstElem && secondUserAnswer != secondElem) {alert("Все неверно");}
+    
+}
+
